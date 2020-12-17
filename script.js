535a046522501ef8fdda49762ba21ba6ba785727
Sondi <anke_sr@hotmail.com>
2020-12-17 10:21:06 +0100

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $("#zero-dropdown").click(function(){
+    $("#zero-content").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#one-dropdown").click(function(){
+    $("#one-content").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#two-dropdown").click(function(){
+    $("#two-content").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#three-dropdown").click(function(){
+    $("#three-content").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#four-dropdown").click(function(){
+    $("#four-content").slideToggle("slow");
+  });
+});
