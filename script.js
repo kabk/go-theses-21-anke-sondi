61e7f6c02386b9a2f29bcfb095391361b1837950
Sondi <anke_sr@hotmail.com>
2021-01-24 21:54:37 +0100

    blur test

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-$(document).ready( function() {
-	$(window).scroll(function() {
-		newNumber=$(window).scrollTop();
-		newNumber=newNumber*myRatio;
-		$('#box').css('-webkit-filter', "blur("+newNumber+"px)");
-		$('#box').css('-moz-filter', "blur("+newNumber+"px)");
-		$('#box').css('-o-filter', "blur("+newNumber+"px)");
-		$('#box').css('-ms-filter', "blur("+newNumber+"px)");
-		$('#box').css('filter', "blur("+newNumber+"px)");
-	});
-
-	myRatio= getRatio(20, 5500); // map the values 0-5500 to 0-20 that is the scroll number range to the blur range desired
-
-	function getRatio(max, max2) {
-		return max/max2;
-	}
-
+$(window).scroll(function() {
+  var heroBlur = 10 - Math.floor($(this).scrollTop() / 100);
+  if (heroBlur < 0) { heroBlur = 0; }
+  $('.alignfull').css({
+    '-webkit-filter': 'blur('+heroBlur+'px)'
+  });
 });
