06208d5bea56259a1812bc85c89871eb4cabd061
Sondi <anke_sr@hotmail.com>
2021-01-24 21:41:04 +0100

    blur test

--- script.js
+++ script.js
@@ -1,48 +1,18 @@
-$(document).ready(function(){
-  $("#abstract-dropdown").click(function(){
-    $("#abstract-content").slideToggle("slow");
-  });
-});
+$(document).ready( function() {
+	$(window).scroll(function() {
+		newNumber=$(window).scrollTop();
+		newNumber=newNumber*myRatio;
+		$('#box').css('-webkit-filter', "blur("+newNumber+"px)");
+		$('#box').css('-moz-filter', "blur("+newNumber+"px)");
+		$('#box').css('-o-filter', "blur("+newNumber+"px)");
+		$('#box').css('-ms-filter', "blur("+newNumber+"px)");
+		$('#box').css('filter', "blur("+newNumber+"px)");
+	});
 
-$(document).ready(function(){
-  $("#content-dropdown").click(function(){
-    $("#content-content").slideToggle("slow");
-  });
-});
+	myRatio= getRatio(20, 5500); // map the values 0-5500 to 0-20 that is the scroll number range to the blur range desired
 
-$(document).ready(function(){
-  $("#intro-dropdown").click(function(){
-    $("#intro-content").slideToggle("slow");
-  });
-});
+	function getRatio(max, max2) {
+		return max/max2;
+	}
 
-$(document).ready(function(){
-  $("#zero-dropdown").click(function(){
-    $("#zero-content").slideToggle("slow");
-  });
-});
-
-$(document).ready(function(){
-  $("#one-dropdown").click(function(){
-    $("#one-content").slideToggle("slow");
-  });
-});
-
-$(document).ready(function(){
-  $("#two-dropdown").click(function(){
-    $("#two-content").slideToggle("slow");
-  });
-});
-
-
-$(document).ready(function(){
-  $("#three-dropdown").click(function(){
-    $("#three-content").slideToggle("slow");
-  });
-});
-
-$(document).ready(function(){
-  $("#four-dropdown").click(function(){
-    $("#four-content").slideToggle("slow");
-  });
 });
